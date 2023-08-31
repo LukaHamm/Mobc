c4800bb482ffeaa20e201e843728e85b7579599a
Nikesplane <nikesplaned@gmail.com>
2023-08-31 12:44:40 +0200

    Test

--- app/src/main/java/app/thecity/activity/ActivityMain.java
+++ app/src/main/java/app/thecity/activity/ActivityMain.java
@@ -36,7 +36,7 @@ import app.thecity.utils.Tools;
 // Hauptaktivität der App
 public class ActivityMain extends AppCompatActivity {
 
-    //Hallo98
+    //Hallo98dsss
     public ActionBar actionBar;
     public Toolbar toolbar;
     private int[] categories;
